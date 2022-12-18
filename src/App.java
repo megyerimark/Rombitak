87b59a1fa22b7c8f99a7e28f80899e1ca969eeb4
Márk Megyeri <megyko1234@gmail.com>
2022-12-18 20:34:08 +0100

    App.java kész

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+/*
+* File: App.java
+* Author: Megyeri Márk Máté
+* Copyright: 2022, Megyeri Márk Máté
+* Group: SZOFT II/N
+* Date: 2022-12-18
+* Github: https://github.com/megyerimark
+* Licenc: GNU GPL
+*/
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new MainController();
     }
 }
