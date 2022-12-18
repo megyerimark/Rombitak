c06c6f47ece162595ef15b7be4f5aafe44726baa
Márk Megyeri <megyko1234@gmail.com>
2022-12-18 20:41:13 +0100

    VolumePanel.java

---
+++ src/views/VolumePanel.java
@@ -0,0 +1,21 @@
+
+/*
+* File: VolumePanel.java
+* Author: Megyeri Márk Máté
+* Copyright: 2022, Megyeri Márk Máté
+* Group: SZOFT II/N
+* Date: 2022-12-18
+* Github: https://github.com/megyerimark
+* Licenc: GNU GPL
+
+*/package views;
+
+public class VolumePanel  extends InputPanel{
+
+    private Object label;
+
+    public void VolumPanel(){
+     this.label.setText("hang");
+    }
+    
+}
